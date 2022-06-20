8e3c746e5908ae5acd60f19ea9b6627d96603a5e
chick_0 <chick_0@ch1ck.xyz>
2022-06-21 01:16:49 +0900

    update build version page

--- src/routes/Build.svelte
+++ src/routes/Build.svelte
@@ -1,21 +1,37 @@
 <script>
     import { HOST } from "../url.js";
-    let host = location.host;
 
-    // 선언되지 않은 변수들은 rollup의 replace플러그인으로 변경됨
+    let remote = new URL("gitRepoURL").pathname.slice(1);
 </script>
 
 <section class="section">
     <div class="container">
+        <h1 class="title is-1">빌드 정보</h1>
+    <!-- svelte-ignore missing-declaration -->
+    {#if isProduction == false}
+        <div class="notification is-danger is-light">
+            해당 빌드는 테스트 모드 입니다.
+        </div>
+    {:else}
+        <div class="notification is-primary is-light">
+            프로덕션 모드 입니다.
+        </div>
+    {/if}
+
         <div class="box">
-            <p>client host = <b>{host}</b></p>
-            <p>api host = <b>{HOST}</b></p>
-            <p>production mode = <b>isProduction</b></p>
+            <h4 class="title is-4">MyPT API</h4>
+            <div class="content is-medium">
+                <p>{HOST}</p>
+            </div>
         </div>
 
         <div class="box">
-            <p>git repo url = <b>gitRepoURL</b></p>
-            <p>last commit = <b>commitId</b></p>
+            <h4 class="title is-4">client version</h4>
+            <div class="content is-medium">
+                <a href="gitRepoURL" target="_blank">
+                    {remote} - commitId
+                </a>
+            </div>
         </div>
     </div>
 </section>
