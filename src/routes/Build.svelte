4af6d54b022cd1e839dd4b7678ac85ec4d2e6385
chick_0 <chick_0@ch1ck.xyz>
2022-06-23 14:32:38 +0900

    check latest commit in build info page

--- src/routes/Build.svelte
+++ src/routes/Build.svelte
@@ -1,12 +1,19 @@
 <script>
+    import { push } from "svelte-spa-router";
     import { HOST } from "../url.js";
 
+    let short = "commitId".slice(0, 7);
+
+    let isGithub = "gitRepoURL".startsWith("https://github.com");
     let remote = new URL("gitRepoURL").pathname.slice(1);
+    if(remote.endsWith(".git"))
+        remote = remote.slice(0, remote.length - 4);
 </script>
 
 <section class="section">
     <div class="container">
         <h1 class="title is-1">빌드 정보</h1>
+        <p class="subtitle has-text-link is-clickable" on:click={() => {push("/")}}>프로젝트 목록으로 돌아가기</p>
     <!-- svelte-ignore missing-declaration -->
     {#if isProduction == false}
         <div class="notification is-danger is-light">
@@ -29,9 +36,28 @@
             <h4 class="title is-4">client version</h4>
             <div class="content is-medium">
                 <a href="gitRepoURL" target="_blank">
-                    {remote} - commitId
+                    {remote} - {short}
                 </a>
             </div>
+            <button
+                class="button is-primary"
+                on:click={() => {
+                    if(!isGithub){
+                        alert("깃헙에 저장된 프로젝트가 아닙니다.")
+                    } else {
+                        fetch(`https://api.github.com/repos/${remote}/commits`).then((resp) => resp.json()).then((json) => {
+                            let top = json[0];
+                            if(top.sha == "commitId"){
+                                alert("최신 버전 입니다.");
+                            } else {
+                                alert("최신 버전이 아닙니다.");
+                            }
+                        });
+                    }
+                }}
+            >
+                check from remote
+            </button>
         </div>
     </div>
 </section>
