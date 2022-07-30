20f3b35c507ee8f34dc249cfde83ceb770e006bd
chick_0 <chick_0@ch1ck.xyz>
2022-07-30 21:32:34 +0900

    add retry button

--- src/Error.svelte
+++ src/Error.svelte
@@ -7,5 +7,12 @@
         <p><b>서버 오류</b>로 인해 프로젝트 정보를 불러오는데 실패했습니다.</p>
         <p>아래의 버튼을 클릭해 서버의 상태를 확인 할 수 있습니다.</p>
     </div>
-    <a class="button is-danger" href="{status_url}" target="_blank"> 서버 상태 확인하기 </a>
+    <div class="buttons">
+        <a class="button is-danger" href="{status_url}" target="_blank">서버 상태 확인하기</a>
+        <button
+            class="button is-primary"
+            on:click="{() => {
+                location.reload();
+            }}">다시 시도하기</button>
+    </div>
 </div>
